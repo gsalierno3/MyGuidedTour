ea0ae32dba36ccc6fcae2e4963a127ba5525a33d
Genaro Salierno <gsalierno3@gatech.edu>
2016-08-13 23:13:46 -0400

    MultifamilyActivity hard coded strings modified

--- app/src/main/java/com/example/android/myguidedtour/MultifamilyActivity.java
+++ app/src/main/java/com/example/android/myguidedtour/MultifamilyActivity.java
@@ -22,14 +22,14 @@ public class MultifamilyActivity extends AppCompatActivity
 
         //create list of projects
         final ArrayList<Project> projects = new ArrayList<Project>();
-        projects.add(new Project("Residential building", R.drawable.multi_federal, 41.008970, -74300426,
-                "21 unit residential complex of two-story dwelling units.  Pompton Lakes, NJ"));
-        projects.add(new Project("90 unit building", R.drawable.multi_rahway,40.609281, -74.268479,
-                "Four story residential over covered parking. Rahway, NJ"));
-        projects.add(new Project("Multifamily study", R.drawable.multi_rutherford,
-                "Multifamily building study on a triangular lot.  Rutherford, NJ"));
-        projects.add(new Project("Townhouses", R.drawable.multi_wallington1,40.858851, -74.116404,
-                "Two-unit townhouse building on a complex with 35 dwelling units. Wallington, NJ"));
+        projects.add(new Project(getString(R.string.residential1_name), R.drawable.multi_federal, 41.008970, -74300426,
+                getString(R.string.residential1_text)));
+        projects.add(new Project(getString(R.string.residential2_name), R.drawable.multi_rahway,40.609281, -74.268479,
+                getString(R.string.residential2_text)));
+        projects.add(new Project(getString(R.string.residential3_name), R.drawable.multi_rutherford,
+                getString(R.string.residential3_text)));
+        projects.add(new Project(getString(R.string.residential4_name), R.drawable.multi_wallington1,40.858851, -74.116404,
+                getString(R.string.residential4_text)));
 
         ProjectAdapter adapter = new ProjectAdapter(this, projects);
 
